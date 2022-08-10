7a266a6a3b60b6501063b1eb52522071d783b569
mephyreji <110896643+mephyreji@users.noreply.github.com>
2022-08-10 15:30:38 +0530

    for_calculator

---
+++ calculator.py
@@ -0,0 +1,24 @@
+print("*****       welcome to meff calculator    *********** ")
+print("enter the number1:")
+num1=int(input())
+print("enter the number2:")
+num2=int(input())
+print("thses are the operation you can:")
+print("1.Addition")
+print("2.subtration")
+print("3.multiplication")
+print("4.division")
+print("5.modulus")
+a=input("please choose an option (1,2,3,4,5):")
+if a=="1":
+    print("this is an an addition")
+    print("the sum of the two number is:",num1+num2)
+if a=="2":
+    print("substration")
+if a=="3":
+    print("multiplication")
+if a=="4":
+    print("division")
+if a=="5":
+    print("modulus")
+
