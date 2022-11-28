3c6eb6b0f99bc66033d672a47cc5b6915a1af956
mephyreji <110896643+mephyreji@users.noreply.github.com>
2022-11-28 14:15:45 +0530

    python dict

---
+++ function.py
@@ -0,0 +1,42 @@
+#block of code usinging for specfic task
+#def function_name()
+#block of code
+#def function_name(para1,para2)
+#block of code
+#def function_name(arg1,arg2)
+#
+#def fun():
+  #  a=10
+ #   b=20
+#    c=a+b
+ #   print(c)
+#fun()
+#argument and parameter
+#def fun(a,b):
+    #c=a+b
+    #print(c)
+#fun(10,20)
+#return
+#def fun(a,b):
+    #c=a+b
+    #return c
+#res=fun(10,20)
+#t=100
+#print(res=t)
+#ARGUMENT TYPES
+def fun(a,b):
+    print(a)
+    print(b)
+
+fun(10,20)
+#arbitary argument=to get the balance in tuple form,space alotationg
+def fun(a,*b):
+    print(a)
+    print(b)
+
+fun(10,20,5,6,4)
+a=(1,2,3)
+print(type(a))
+
+
+
